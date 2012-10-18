7a2f5d077f37cf8f974d923e4458c75289b29fed
Dimitris Zervas <01ttouch@gmail.com>
2012-10-18 04:59:44 +0300

    Fixed deprecaded functions in glue

--- misc/glue/server.lua
+++ misc/glue/server.lua
@@ -1,12 +1,12 @@
 function gluePlayer(slot, vehicle, x, y, z, rotX, rotY, rotZ)
-	attachElementToElement(source, vehicle, x, y, z, rotX, rotY, rotZ)
-	setPlayerWeaponSlot(source, slot)
+	attachElements(source, vehicle, x, y, z, rotX, rotY, rotZ)
+	setPedWeaponSlot(source, slot)
 end
 addEvent("gluePlayer",true)
 addEventHandler("gluePlayer",getRootElement(),gluePlayer)
 
 function ungluePlayer()
-	detachElementFromElement(source)
+	detachElements(source)
 end
 addEvent("ungluePlayer",true)
 addEventHandler("ungluePlayer",getRootElement(),ungluePlayer)
